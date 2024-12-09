ae3dd5e11fe0d7035c27cd46fdedbd513df9dda0
cyph3rasi <cmcelvogue91@gmail.com>
2024-12-09 13:08:25 -0800

    Add test endpoint and update routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,18 @@ const authRoutes = require('./auth.routes');
 const postRoutes = require('./post.routes');
 const userRoutes = require('./user.routes');
 
+// Basic test endpoint
+router.get('/', (req, res) => {
+  res.json({
+    message: 'Welcome to Project Rever API',
+    endpoints: {
+      auth: '/api/auth',
+      posts: '/api/posts',
+      users: '/api/users'
+    }
+  });
+});
+
 // Route middleware
 router.use('/auth', authRoutes);
 router.use('/posts', postRoutes);
