96aea126992dcaa08a00dc1006e83e013c2605b1
cyph3rasi <cmcelvogue91@gmail.com>
2024-12-09 14:16:25 -0800

    Update index.js

--- src/client/src/index.js
+++ src/client/src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Create root element
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render app
 root.render(
   <React.StrictMode>
     <App />
